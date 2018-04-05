0a499b8464dae7662abac373567c66c54109c318
Mexahoid <mirramax97@Mail.ru>
2018-04-05 16:00:31 +0300

    Initial commit

---
+++ src/main/java/ModuleInterface.java
@@ -0,0 +1,16 @@
+public interface ModuleInterface {
+
+    boolean CreateStudent(String name, int groupnum, int coursenum);
+    boolean DeleteStudent(String name, int groupnum, int coursenum);
+
+    boolean CreateGroup(int groupnum, int coursenum);
+    boolean DeleteGroup(int groupnum, int coursenum);
+
+    boolean CreateCourse(int coursenum);
+    boolean DeleteCourse(int coursenum);
+
+    void SeeCourses();
+    void SeeCourseGroups(int coursenum);
+    void SeeGroupStudents(int groupnum, int coursenum);
+    
+}
